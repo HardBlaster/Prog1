a53d1bb86c5c5ca100d7c1263b80cecbbc37b672
HardBlaster <33747335+HardBlaster@users.noreply.github.com>
2018-02-13 22:35:03 +0100

    Add files via upload

---
+++ valtozo.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "a= ";
+    cin >> a;
+    cout << "\n" << "b= ";
+    cin >> b;
+    cout << "\n";
+    a = b - a;
+    b = b - a;
+    a = b + a;
+    cout << "Kivonással:\n" << "a= " << a << "\nb= " << b << "\n";
+    a = a * b;
+    b = a / b;
+    a = a / b;
+    cout << "Szorzás-osztással:\n" << "a= " << a << "\nb= " << b;
+    return 0;
+}
